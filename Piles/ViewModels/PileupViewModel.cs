35e78c9271db0734330dc39f2be90cb1e05ab782
ircjwin <christopherjamesirwin@gmail.com>
2025-02-03 20:25:08 -0800

    feat: add new tab button

--- Piles/ViewModels/PileupViewModel.cs
+++ Piles/ViewModels/PileupViewModel.cs
@@ -6,6 +6,31 @@ namespace Piles.ViewModels
 {
     public class PileupViewModel : ViewModelBase
     {
+        private bool _isInserting { get; set; } = false;
+
+        private int _currentIndex;
+        public int CurrentIndex
+        {
+            get
+            {
+                return _currentIndex;
+            }
+            set
+            {
+                if (_isInserting)
+                {
+                    _isInserting = false;
+                    return;
+                }
+                if (value == _piles.Count - 1)
+                {
+                    AddPile();
+                }
+                _currentIndex = value;
+                OnPropertyChanged(nameof(CurrentIndex));
+            }
+        }
+
         private readonly Pileup _pileup;
 
         private readonly ObservableCollection<PileViewModel> _piles;
@@ -28,6 +53,19 @@ namespace Piles.ViewModels
                 PileViewModel pileViewModel = new PileViewModel(pile);
                 _piles.Add(pileViewModel);
             }
+
+            Pile addPile = new Pile("+", new ObservableCollection<Rumination>());
+            PileViewModel addPileViewModel = new PileViewModel(addPile);
+            _piles.Add(addPileViewModel);
+        }
+
+        private void AddPile()
+        {
+            Pile newPile = new Pile("New List", new ObservableCollection<Rumination>());
+            PileViewModel newPileViewModel = new PileViewModel(newPile);
+            _isInserting = true;
+            _piles.Insert(_piles.Count - 1, newPileViewModel);
+            CurrentIndex = _piles.Count - 2;
         }
     }
 }
