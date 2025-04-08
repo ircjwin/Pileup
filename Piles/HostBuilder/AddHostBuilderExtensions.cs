2863333b7183f621f3bacb32c6c761ef4344e53e
ircjwin <christopherjamesirwin@gmail.com>
2025-04-07 20:27:03 -0700

    chore: update packages

--- Piles/HostBuilder/AddHostBuilderExtensions.cs
+++ Piles/HostBuilder/AddHostBuilderExtensions.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Piles.Models;
-using Piles.Services;
 using Piles.ViewModels;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Piles.HostBuilder
 {
@@ -16,20 +10,9 @@ namespace Piles.HostBuilder
         {
             hostBuilder.ConfigureServices(services =>
             {
-                services.AddSingleton(async (s) => await CreatePileupViewModel(s));
-
                 services.AddTransient<PileViewModel>();
-                services.AddTransient<Func<Pile, PileViewModel>>((s) => (pile) => new PileViewModel(pile, s.GetRequiredService<IRuminationService>()));
             });
             return hostBuilder;
-        }
-
-        private static async  Task<PileupViewModel> CreatePileupViewModel(IServiceProvider services)
-        {
-            IPileService pileService = services.GetService<IPileService>();
-            ICollection<Pile> piles = await pileService.GetAllPiles();
-            Pileup pileup = new Pileup(piles);
-            return new PileupViewModel(pileup);
         }
     }
 }
---
+++ Piles/Migrations/20250408032201_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Piles.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Piles",
+                columns: table => new
+                {
+                    Origin = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piles", x => new { x.Origin, x.CreatedOn });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ruminations",
+                columns: table => new
+                {
+                    Origin = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    PileOrigin = table.Column<int>(type: "INTEGER", nullable: false),
+                    PileCreatedOn = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ruminations", x => new { x.Origin, x.CreatedOn });
+                    table.ForeignKey(
+                        name: "FK_Ruminations_Piles_PileOrigin_PileCreatedOn",
+                        columns: x => new { x.PileOrigin, x.PileCreatedOn },
+                        principalTable: "Piles",
+                        principalColumns: new[] { "Origin", "CreatedOn" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ruminations_PileOrigin_PileCreatedOn",
+                table: "Ruminations",
+                columns: new[] { "PileOrigin", "PileCreatedOn" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ruminations");
+
+            migrationBuilder.DropTable(
+                name: "Piles");
+        }
+    }
+}
