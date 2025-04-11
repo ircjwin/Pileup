e2139392315f7c17d52fd06cdc9099e3fe5ab21f
ircjwin <christopherjamesirwin@gmail.com>
2025-04-10 23:49:49 -0700

    feat: add textbox text selection on doubleclick

---
+++ Piles/AttachedProperties/FocusExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Piles.AttachedProperties
+{
+    public static class FocusExtension
+    {
+        public static readonly DependencyProperty IsFocusedProperty = 
+            DependencyProperty.RegisterAttached("IsFocused", typeof(bool), typeof(FocusExtension), new UIPropertyMetadata(false, OnIsFocusedPropertyChanged));
+
+        public static bool GetIsFocused(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsFocusedProperty);
+        }
+
+        public static void SetIsFocused(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsFocusedProperty, value);
+        }
+
+        private static void OnIsFocusedPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var uie = d as TextBox;
+            if ((bool)e.NewValue)
+            {
+                uie.Focus();
+                uie.SelectAll();
+            }
+        }
+    }
+}
