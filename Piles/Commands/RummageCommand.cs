cdee609623390b36f05d75c43d173d840437f2e2
ircjwin <christopherjamesirwin@gmail.com>
2025-05-13 14:46:58 -0700

    fix: handle empty rummage list

--- Piles/Commands/RummageCommand.cs
+++ Piles/Commands/RummageCommand.cs
@@ -20,12 +20,17 @@ namespace Piles.Commands
                 IList<RuminationViewModel> rummage = new List<RuminationViewModel>();
                 foreach (RuminationViewModel ruminationViewModel in _pileViewModel.Ruminations)
                 {
-                    ruminationViewModel.IsRummagePick = false;
+                    if (ruminationViewModel.IsRummagePick)
+                    {
+                        ruminationViewModel.IsRummagePick = false;
+                        continue;
+                    }
                     if (ruminationViewModel.IsRummage)
                     {
                         rummage.Add(ruminationViewModel);
                     }
                 }
+                if (rummage.Count == 0) return;
                 Random random = new Random();
                 int rummageIndex = random.Next(rummage.Count);
                 rummage[rummageIndex].IsRummagePick = true;
--- Piles/ViewModels/MainViewModel.cs
+++ Piles/ViewModels/MainViewModel.cs
@@ -1,15 +1,38 @@
-﻿namespace Piles.ViewModels
+﻿using System.ComponentModel;
+
+namespace Piles.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
         public CommandStackViewModel CommandStackViewModel { get; }
         public PileupViewModel PileupViewModel { get; }
-        public PileViewModel CurrentPileViewModel => PileupViewModel.TopPile;
+
+        private PileViewModel _currentPileViewModel;
+        public PileViewModel CurrentPileViewModel
+        { 
+            get {  return _currentPileViewModel; }
+            set
+            {
+                _currentPileViewModel = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainViewModel(PileupViewModel pileupViewModel, CommandStackViewModel commandStackViewModel)
         {
             PileupViewModel = pileupViewModel;
             CommandStackViewModel = commandStackViewModel;
+            CurrentPileViewModel = PileupViewModel.TopPile;
+
+            PileupViewModel.PropertyChanged += OnPileupViewModelPropertyChanged;
+        }
+
+        private void OnPileupViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(PileupViewModel.TopPile))
+            {
+                CurrentPileViewModel = PileupViewModel.TopPile;
+            }
         }
     }
 }
