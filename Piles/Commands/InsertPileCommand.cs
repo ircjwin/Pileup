ebd48d33081672c9f972ed2c81f5b31bf722eaae
ircjwin <christopherjamesirwin@gmail.com>
2025-05-26 19:43:57 -0700

    feat: add insert commands for Pile and Rumination

---
+++ Piles/Commands/InsertPileCommand.cs
@@ -0,0 +1,66 @@
+﻿using Piles.Models;
+using Piles.ViewModels;
+using System.Linq;
+
+namespace Piles.Commands
+{
+    public class InsertPileCommand : UndoableCommandBase
+    {
+        private readonly Pileup _pileup;
+
+        private OperationType _operationType = OperationType.Add;
+        public override OperationType OperationType
+        {
+            get { return _operationType; }
+        }
+
+        private Pile _target;
+        public override Pile Target
+        {
+            get { return _target; }
+        }
+
+        private TargetType _targetType = TargetType.Pile;
+        public override TargetType TargetType
+        {
+            get { return _targetType; }
+        }
+
+        public InsertPileCommand(Pileup pileup, Pile pile)
+        {
+            _pileup = pileup;
+            _target = pile;
+        }
+
+        public InsertPileCommand(Pileup pileup, ICommandListener commandListener)
+        {
+            _pileup = pileup;
+
+            commandListener.Listen(this);
+        }
+
+        public override void Execute(object parameter)
+        {
+            _pileup.AddPile();
+            _target = _pileup.Piles.LastOrDefault();
+
+            OnExecuted();
+        }
+
+        public override void Redo()
+        {
+            _pileup.AddPile();
+        }
+
+        public override void Undo()
+        {
+            _pileup.RemovePileAt(_pileup.Piles.Count - 1);
+        }
+
+        public override InsertPileCommand Clone()
+        {
+            return new InsertPileCommand(_pileup, _target);
+        }
+    }
+}
+
---
+++ Piles/Commands/InsertRuminationCommand.cs
@@ -0,0 +1,72 @@
+﻿using Piles.Models;
+using Piles.ViewModels;
+using System;
+using System.Linq;
+
+namespace Piles.Commands
+{
+    public class InsertRuminationCommand : UndoableCommandBase
+    {
+        private readonly Pile _pile;
+
+        private OperationType _operationType = OperationType.Add;
+        public override OperationType OperationType
+        {
+            get { return _operationType; }
+        }
+
+        private Tuple<Rumination, Pile> _target;
+        public override Tuple<Rumination, Pile> Target
+        {
+            get { return _target; }
+        }
+
+        private TargetType _targetType = TargetType.Rumination;
+        public override TargetType TargetType
+        {
+            get { return _targetType; }
+        }
+
+        public InsertRuminationCommand(Pile pile, Tuple<Rumination, Pile> ruminationPile)
+        {
+            _pile = pile;
+            _target = ruminationPile;
+        }
+
+        public InsertRuminationCommand(Pile pile, ICommandListener commandListener)
+        {
+            _pile = pile;
+
+            commandListener.Listen(this);
+        }
+
+        public override void Execute(object parameter)
+        {
+            PileViewModel pileViewModel = parameter as PileViewModel;
+            string newRuminationDescription = pileViewModel.NewRuminationDescription;
+            _pile.AddRumination(newRuminationDescription);
+
+            _target = new Tuple<Rumination, Pile>(_pile.Ruminations.LastOrDefault(), _pile);
+            pileViewModel.NewRuminationDescription = "";
+
+            OnExecuted();
+        }
+
+        public override void Redo()
+        {
+            (Rumination rumination, Pile pile) = _target;
+            _pile.AddRumination(rumination);
+        }
+
+        public override void Undo()
+        {
+            _pile.RemoveRuminationAt(_pile.Ruminations.Count - 1);
+        }
+
+        public override InsertRuminationCommand Clone()
+        {
+            return new InsertRuminationCommand(_pile, _target);
+        }
+    }
+}
+
